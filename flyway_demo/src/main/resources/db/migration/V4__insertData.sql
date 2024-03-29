62451fb61a55f0fbb110c80ed31b9c3599f72b72
rberkouch <140326672+rberkouch@users.noreply.github.com>
2024-03-29 12:56:27 +0100

    Add files via upload

---
+++ flyway_demo/src/main/resources/db/migration/V4__insertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO  REGISTRATION_USER (username, first_name, last_name,email,mobile)
+VALUES ('javaTech','java','tech','a@gmail.com','21457898');
+INSERT INTO  REGISTRATION_USER (username, first_name, last_name,email,mobile)
+VALUES ('C','C++','tech','C@gmail.com','32658745');
---
+++ flyway_demo/src/test/java/fr/intiformation/flyway_demo/FlywayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.intiformation.flyway_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
