738b630339e08960c1937a8928b346a317b0d7d4
rberkouch <140326672+rberkouch@users.noreply.github.com>
2024-04-11 15:55:35 +0200

    Add files via upload

---
+++ springBoot-MongoDB/springboot-mongo-atlas/src/test/java/fr/intiformaion/SpringbbotMongoAtlasApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.intiformaion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbbotMongoAtlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
